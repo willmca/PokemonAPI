79fb47e44d7d1a3202de6b0feda3bb26e325bed9
Will McAllister <willmca32@gmail.com>
2019-09-12 16:48:58 -0400

    Basic requirements met, first version done

--- controllers/pokemon.js
+++ controllers/pokemon.js
@@ -1,65 +1,69 @@
 const Pokemon = require("../db/models/Pokemon");
 
 module.exports = {
-    index: (req, res) => {
-      Pokemon.find({}).then(pokemon => {
+  index: (req, res) => {
+    Pokemon.find({})
+    .populate("generation", "name")
+      .then(pokemon => {
         res.json(pokemon);
       });
-    },
-    indexByName: (req,res) => {
-      Pokemon.findOne({
-        name: req.params.name
-      }).then(pokemon =>{
+  },
+  indexByName: (req, res) => {
+    Pokemon.findOne({
+      name: req.params.name
+    }).then(pokemon => {
       res.json(pokemon)
-      })
-    },
-    indexByType: (req,res) => {
-      Pokemon.find({
-        type:req.params.type
-      }).then(pokemon =>{
-        res.json(pokemon)
-      })
-    },
-    indexByNumber: (req,res) => {
-      Pokemon.find({
-        number:req.params.number
-      }).then(pokemon => {
-        res.json(pokemon)
-      })
-    },
-    deleteByName: (req,res) => {
-      Pokemon.deleteOne({
-        name:req.params.name
-      }).then(pokemon =>{
-        res.json(pokemon)
-      })
-    },
-    deleteByNumber: (req,res) => {
-      Pokemon.deleteOne({
-        number:req.params.number
-      }).then(pokemon =>{
-        res.json(pokemon)
-      })
-    },
-    updateByName: (req,res) => {
-      Pokemon.findOneAndUpdate({
-        name:req.params.name
-      }, req.body)
+    })
+  },
+  indexByType: (req, res) => {
+    Pokemon.find({
+      type: req.params.type
+    }).then(pokemon => {
+      res.json(pokemon)
+    })
+  },
+  indexByNumber: (req, res) => {
+    Pokemon.find({
+      number: req.params.number
+    }).then(pokemon => {
+      res.json(pokemon)
+    })
+  },
+  deleteByName: (req, res) => {
+    Pokemon.deleteOne({
+      name: req.params.name
+    }).then(pokemon => {
+      res.json(pokemon)
+    })
+  },
+  deleteByNumber: (req, res) => {
+    Pokemon.deleteOne({
+      number: req.params.number
+    }).then(pokemon => {
+      res.json(pokemon)
+    })
+  },
+  updateByName: (req, res) => {
+    Pokemon.findOneAndUpdate({
+      name: req.params.name
+    }, req.body,
+    { new: true })
       .then(pokemon => res.json(pokemon))
-    },
-    updateByNumber: (req,res) => {
-      Pokemon.findOneAndUpdate({
-        number:req.params.number
-      }, req.body)
+  },
+  updateByNumber: (req, res) => {
+    Pokemon.findOneAndUpdate({
+      number: req.params.number
+    }, req.body,
+    { new: true })
       .then(pokemon => res.json(pokemon))
-    },
-      create: (req, res) => {
-      Pokemon.create(req.body)
-        .then(pokemon => {
-          res.json(pokemon)
-        })
-    }
-    }
-   
-  
-    //TO DO. Make pokemon always display in order by number, fix postman returns for put
+  },
+  create: (req, res) => {
+    Pokemon.create(req.body)
+      .then(pokemon => {
+        res.json(pokemon)
+      })
+  }
+}
+
+
+    
--- controllers/type.js
+++ controllers/type.js
@@ -1,36 +1,40 @@
 const Type = require("../db/models/Type");
 
 module.exports = {
-    index: (req, res) => {
-      Type.find({}).then(type => {
-        res.json(type);
-      });
-    },
-    indexByName: (req,res) => {
-      Type.findOne({
-        name: req.params.name
-      }).then(type =>{
-      res.json(type)
-      })
-},
-    delete: (req,res) => {
-      Type.deleteOne({
-        name: req.params.name
-      }).then(type =>{
-        res.json(type)
-      })
-    },
-    updateByName: (req,res) => {
-      Type.findOneAndUpdate({
-        name:req.params.name
-      }, req.body)
-      .then(type => res.json(type))
-},
-    create: (req, res) => {
-      Type.create(req.body)
-      .then(type => {
+  index: (req, res) => {
+    Type.find({}).then(type => {
+      res.json(type);
+    });
+  },
+  indexByName: (req, res) => {
+    Type.findOne({
+      name: req.params.name
+    }).then(type => {
       res.json(type)
     })
-},
+  },
+  delete: (req, res) => {
+    Type.deleteOne({
+      name: req.params.name
+    }).then(type => {
+      res.json(type)
+    })
+  },
+  updateByName: (req, res) => {
+    console.log(req.params.name)
+    Type.findOneAndUpdate(
+    {
+      name: req.params.name
+    },
+    req.body,
+    { new: true })
+      .then(type => res.json(type))
+  },
+  create: (req, res) => {
+    Type.create(req.body)
+      .then(type => {
+        res.json(type)
+      })
+  },
 
 }
